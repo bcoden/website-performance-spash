1fe54a4dd4d9f6fc8a90f5e473980721c89c5bbb
Joe McCorison <joemccorison@gmail.com>
2020-12-20 23:11:18 +0000

    fixes bug

--- app/View/Components/website-performance-stats.php
+++ app/View/Components/website-performance-stats.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class website-performance-stats extends Component
+class websitePerformanceStats extends Component
 {
     /**
      * Create a new component instance.
