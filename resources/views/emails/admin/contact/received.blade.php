ef546624140d95b0623e3815511d257064e56fd4
Joe McCorison <joemccorison@gmail.com>
2020-12-20 23:06:00 +0000

    adds translations

--- resources/views/emails/admin/contact/received.blade.php
+++ resources/views/emails/admin/contact/received.blade.php
@@ -1,2 +1,2 @@
-<h1>Hello {{ $user->name }},</h1>
-<p>You have recieved a new inquiry, please visit the dashboard for more information.</p>
+<h1>{{ __('Hello') }} {{ $user->name }},</h1>
+<p>{{ __('You have recieved a new inquiry, please visit the dashboard for more information.') }}</p>
--- resources/views/emails/guest/contact.blade.php
+++ resources/views/emails/guest/contact.blade.php
@@ -1,2 +1,2 @@
-<h1>Hello {{ $user->name }},</h1>
-<p>Thank you for your inquiry, we will get back to you as soon as possible.</p>
+<h1>{{ __('Hello') }} {{ $user->name }},</h1>
+<p>{{ __('Thank you for your inquiry, we will get back to you as soon as possible.') }}</p>
