b1621728a2f399d458935fa9da100c8b73b589a0
Joe McCorison <joemccorison@gmail.com>
2020-12-21 15:51:46 +0000

    adds admin name to emails

--- resources/views/emails/admin/contact/received.blade.php
+++ resources/views/emails/admin/contact/received.blade.php
@@ -1,2 +1,2 @@
 <h1>{{ __('Hello') }} {{ $user->name }},</h1>
-<p>{{ __('You have recieved a new inquiry, please visit the dashboard for more information.') }}</p>
+<p>{{ __('You have received a new inquiry, please visit the dashboard for more information.') }}</p>
