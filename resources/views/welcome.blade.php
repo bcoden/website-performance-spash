c5f1fec2a984c6bbba1d40ad967573f8e1cec082
Joe McCorison <joemccorison@gmail.com>
2020-12-21 08:52:03 +0000

    adds meta tags, title and desciption

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="User experience is the difference between your success and failure on the web. Use our tool to evaluate your websites user experience.">
+    <meta name="robots" content="index, follow">
+    <link rel="canonical" href="{{ env('APP_URL') }}" />
+
+    <title>Website Performance Score</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
