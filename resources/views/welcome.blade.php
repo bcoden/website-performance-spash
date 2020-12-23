497595d97543eecdb0618325fa4b38386d280b86
Joe McCorison <joemccorison@gmail.com>
2020-12-23 21:20:14 +0000

    adds favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="User experience is the difference between your success and failure on the web. Use our tool to evaluate your websites user experience.">
     <meta name="robots" content="index, follow">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
     <link rel="canonical" href="{{ env('APP_URL') }}" />
 
     <title>Website Performance Score</title>
